eebcf799e7b1e57e5ca99612342cb05e0ea0b094
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2018-11-17 09:43:48 -0500

    Changed game size

--- run_game.bat
+++ run_game.bat
@@ -1,1 +1,1 @@
-halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "python halbot_ml.py" "python halbot_ml.py"
+halite.exe --replay-directory replays/ -vvv --width 40 --height 40 "python halbot_ml.py" "python halbot_ml.py"
